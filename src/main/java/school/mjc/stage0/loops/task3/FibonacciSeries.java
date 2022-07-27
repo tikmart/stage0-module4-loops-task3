34f9035fed189d9f64cf3cc1904491efb55a3d4e
tikmart <t.martirosyan012@gmail.com>
2022-07-27 22:32:41 +0400

    second commit

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -1,6 +1,28 @@
 package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
-    public void printFibonacci(int lastFibonacci) {
+    public static void printFibonacci(int lastFibonacci) {
+
+        int numOne = 0;
+        int numTwo = 1;
+        for (int i = 1; i <= lastFibonacci ; ++i) {
+
+            System.out.println(numOne);
+
+            int sumOfPrevTwo = numOne + numTwo;
+            numOne = numTwo;
+            numTwo = sumOfPrevTwo;
+
+
+
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        printFibonacci(3);
+        printFibonacci(5);
+
     }
 }
